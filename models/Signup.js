764ce7143e8de79f75d244c590b40956f8700085
khwinana <austinkhwinana23@gmail.com>
2023-09-13 10:06:58 +0200

    changes of model

--- models/Signup.js
+++ models/Signup.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
 
 const User = new mongoose.Schema({
-  ussername: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  verify: { type: String, required: true },
+username: { type: String, required: true },
+password: { type: String, required: true },
+verify: { type: String, required: true },
 });
 
 module.exports = mongoose.model('user', User);
