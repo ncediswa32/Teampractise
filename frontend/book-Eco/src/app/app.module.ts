a2bd3d4b3eaa57a860e926b27e3a916d8056ab74
ncediswa32 <ncediswatabule.tabule399@gmail.com>
2023-09-13 14:41:59 +0200

    cards

--- frontend/book-Eco/src/app/app.module.ts
+++ frontend/book-Eco/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
